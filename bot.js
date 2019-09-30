9cdc0ae8c7c951fffb07dfe24ca78340536931be
arora-ria <arora09ria@gmail.com>
2019-09-30 12:09:32 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(context.activity.text);
+            await context.sendActivity(context.activity.text.split('').reverse().join(''));
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
